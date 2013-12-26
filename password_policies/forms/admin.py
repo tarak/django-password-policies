0837f72d0b27a09388e7d9dc37a802e3ef978ec8
Tarak Blah <halbkarat@gmail.com>
2013-12-26 16:01:15 +0100

    Added import of ugettext to password_policies.forms.admin.

--- password_policies/forms/admin.py
+++ password_policies/forms/admin.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AdminPasswordChangeForm
+from django.utils.translation import ugettext as _
 
 from password_policies.conf import settings
 from password_policies.forms.fields import PasswordPoliciesField
