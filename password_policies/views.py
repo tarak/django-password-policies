b1619256af6ce6ef6e1166ef10f7d39bc36bf516
Brian May <brian.may@wspdigital.com>
2016-07-01 15:11:30 +1000

    Use GET instead of REQUEST
    REQUEST is not defined in Django 1.9.x

--- password_policies/views.py
+++ password_policies/views.py
@@ -108,7 +108,7 @@ if set, otherwise the URL to the :class:`PasswordChangeDoneView`.
 
     def get_context_data(self, **kwargs):
         name = self.redirect_field_name
-        kwargs[name] = self.request.REQUEST.get(name, '')
+        kwargs[name] = self.request.GET.get(name, '')
         return super(PasswordChangeFormView, self).get_context_data(**kwargs)
 
 
